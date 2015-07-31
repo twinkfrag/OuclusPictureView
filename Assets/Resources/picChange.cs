d5483bce83014551a6a25d29d4999d4234dd101b
あると <mail@twinkfrag.net>
2015-07-31 16:32:38 +0900

    2014学祭で4.6fで作ったけど間違えて5.1で中途半端に開いてしまった

---
+++ Assets/Resources/picChange.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+public class picChange : MonoBehaviour
+{
+
+	List<Texture2D> pics;
+	GameObject target;
+	string dir = @"./img/";
+
+	// Use this for initialization
+	void Start()
+	{
+		target = GameObject.Find("imgPlane");
+		pics = new List<Texture2D>();
+
+		var source = new DirectoryInfo(dir);
+		if (!source.Exists) source.Create();
+		foreach (var file in source
+			.GetFiles().Where(x => 
+				string.Compare(x.Extension, ".jpg", true) == 0 || 
+				string.Compare(x.Extension, ".png", true) == 0))
+		{
+			var tex = new Texture2D(0, 0);
+			tex.LoadImage(LoadBin(file.FullName));
+			pics.Add(tex);
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (pics != null)
+		{
+			var pic = pics[(int)(Time.time / 10) % pics.Count];
+			var height = pic.height;
+			target.GetComponent<Renderer>().material.mainTexture = pic;
+			//target.renderer.material.shader = Shader.Find("Transparent/Diffuse");
+			target.transform.localScale = new Vector3((float)pic.width / pic.height, 1.0f, 1.0f);
+		}
+	}
+
+	byte[] LoadBin(string path)
+	{
+		byte[] buf;
+		using (FileStream fs = new FileStream(path, FileMode.Open))
+		using (BinaryReader br = new BinaryReader(fs))
+		{
+			buf = br.ReadBytes((int)br.BaseStream.Length);
+		}
+		return buf;
+	}
+}
